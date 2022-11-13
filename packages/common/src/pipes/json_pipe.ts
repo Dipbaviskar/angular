2a6991bdee16eec4eb36c849707e09a619c05b6e
Dipak baviskar <106143172+Dipbaviskar@users.noreply.github.com>
2022-11-13 11:32:51 +0530

    docs(common): describe your change...

--- packages/common/src/pipes/json_pipe.ts
+++ packages/common/src/pipes/json_pipe.ts
@@ -32,7 +32,6 @@ export class JsonPipe implements PipeTransform {
   /**
    * @param value A value of any type to convert into a JSON-format string.
    */
-  transform(value: any): string {
-    return JSON.stringify(value, null, 2);
+
   }
 }
